7a1a603c396c2861d39c0fe87237556c87dcceab
aaronub <wo-08@hotmail.com>
2022-09-14 12:27:16 -0400

    typo process.env.DATABASE_URL

--- server/db/database.js
+++ server/db/database.js
@@ -12,7 +12,7 @@ console.log(chalk.yellow('Opening database connection'))
 
 // create the database instance that can be used in other database files
 const db = new Sequelize(
-  process.env.DATABAE_URL ||
+  process.env.DATABASE_URL ||
   `postgres://localhost:5432/${pkg.name}`, {
   logging: false // so we don't see all the SQL queries getting made
 })
